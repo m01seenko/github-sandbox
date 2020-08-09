fa3915e8470b3bbd3e7f7c4be7876dfb59ba595f
Andrei Moiseenko <amoiseenko@reg.ru>
2020-08-09 23:37:43 +0300

    Чиним ругань flake8

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,13 +1,15 @@
 from sandbox import add
 
+
 def test_1_plus_2_equal_3_ok():
     a = 1
     b = 2
     c = add(a, b)
     assert c == 3
 
+
 def test_5_plus_3_equal_9_fail():
     a = 5
     b = 3
     c = add(a, b)
-    assert c != 9 
+    assert c != 9
